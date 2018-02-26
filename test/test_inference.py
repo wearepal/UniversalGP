772368385da65d91318331f9be982e15213b967a
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-02-26 18:41:29 +0000

    Improve test of LOO loss

--- test/test_inference.py
+++ test/test_inference.py
@@ -27,7 +27,7 @@ def test_variational_complete():
     # construct objects
     likelihood = lik.LikelihoodGaussian(1.0)
     kernel = [cov.SquaredExponential(input_dim=1, length_scale=0.5, sf=1.0)]
-    vi = inference.Variational(num_samples=100000, lik_func=likelihood, cov_func=kernel, num_components=1,
+    vi = inference.Variational(num_samples=5000000, lik_func=likelihood, cov_func=kernel, num_components=1,
                                optimize_inducing=False, use_loo=True)
 
     # compute losses and predictions
@@ -38,7 +38,7 @@ def test_variational_complete():
 
     # check results
     np.testing.assert_almost_equal(nelbo.numpy(), 4.1, decimal=1)
-    np.testing.assert_almost_equal(loo.numpy(), 9.0, decimal=0)
+    np.testing.assert_allclose(loo.numpy(), 9.9, rtol=0.01)  # test with a relative tolerance of 1%
     np.testing.assert_almost_equal(pred_mean.numpy(), 0.0, SIG_FIGS)
     np.testing.assert_almost_equal(tf.squeeze(pred_var).numpy(), 2.0, decimal=3)
 
