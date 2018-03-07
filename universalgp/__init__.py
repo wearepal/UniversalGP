2524ac6dc36944ad53b1155ef6c0a3da1d6a8f19
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-03-07 16:15:08 +0000

    Remove old code
    With 2 new better training loops available, `gp.py` is not needed
    anymore. Without `gp.py`, the old examples don't work anymore and the
    `Dataset` class is not needed.

--- universalgp/__init__.py
+++ universalgp/__init__.py
@@ -6,12 +6,10 @@ Created on Tue Jan 23 18:08:14 2018
 @author: zc223
 """
 
-from .gp import GaussianProcess
 from . import cov
 from . import mean
 from . import lik
 from . import inf
 from . import util
-from . import datasets
 from . import train_graph
 from . import train_eager
