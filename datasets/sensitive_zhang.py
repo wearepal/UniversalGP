f2fab6280186ae591c4dbc85711a26777be30eaa
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-04-19 18:03:43 +0100

    Adapt datasets and add 2 new ones
    * modify `sensitive` to have the sensitive attribute as part of the
      input
    * add another variant of `sensitive_zhang`
    * add `flipped_labels` which is what the ybar theory expects from a
      dataset

--- datasets/sensitive_zhang.py
+++ datasets/sensitive_zhang.py
@@ -1,10 +1,10 @@
-"""Synthetic dataset with bias"""
+"""Toy dataset with a sensitive attribute"""
 import numpy as np
 from numpy import random
 
-from .definition import Dataset, select_training_and_test, to_tf_dataset_fn
+from .definition import Dataset, select_training_and_test, to_tf_dataset_fn, sensitive_statistics
 
-SEED = 1234
+SEED = 123
 
 
 def sensitive_zhang(flags):
@@ -12,15 +12,15 @@ def sensitive_zhang(flags):
     Toy dataset from Zhang (2017) 'Mitigating Unwanted Biases with Adversarial Learning'
     """
     # parameters
-    n_all = 1000
-    num_train = 750
-    num_inducing = flags['num_inducing']  # 250
-    latent_std = 2.  # standard dev of the latent variable that we're trying to predict. bigger -> easier to predict
-    features_std = .5  # standard dev of the features with respect to latent var. smaller -> easier to predict
-    raw_output_std = .5  # standard dev of the raw output with respect to latent var. smaller -> easier to predict
+    n_all = 3000
+    num_train = 2000
+    num_inducing = flags['num_inducing']  # 500
+    latent_std = 1.  # standard dev of the latent variable that we're trying to predict. bigger -> easier to predict
+    features_std = 1.  # standard dev of the features with respect to latent var. smaller -> easier to predict
+    raw_output_std = 1.  # standard dev of the raw output with respect to latent var. smaller -> easier to predict
 
     # construction
-    np.random.seed(SEED)
+    np.random.seed(SEED)  # make it reproducible
     sensitive_attr = random.random_integers(0, 1, n_all)
     latent = random.normal(sensitive_attr, latent_std)
     features = random.normal(latent, features_std)
@@ -30,15 +30,61 @@ def sensitive_zhang(flags):
     (xtrain, ytrain, strain), (xtest, ytest, stest) = select_training_and_test(
         num_train, features[..., np.newaxis], labels[..., np.newaxis], sensitive_attr[..., np.newaxis])
 
+    sensitive_statistics(ytrain, strain, ytest, stest)
+
     return Dataset(
         train_fn=to_tf_dataset_fn(xtrain, ytrain, strain),
         test_fn=to_tf_dataset_fn(xtest, ytest, stest),
         num_train=num_train,
-        input_dim=1,
-        inducing_inputs=xtrain[::num_train // num_inducing],
+        input_dim=2,
+        inducing_inputs=np.concatenate((xtrain[::num_train // num_inducing], strain[::num_train // num_inducing]), -1),
         output_dim=1,
         lik="LikelihoodLogistic",
-        metric="logistic_accuracy",
+        metric="logistic_accuracy,pred_rate_y1_s0,pred_rate_y1_s1,base_rate_y1_s0,base_rate_y1_s1",
+        xtrain=xtrain,
+        ytrain=ytrain,
+        xtest=xtest,
+        ytest=ytest,
+        strain=strain,
+        stest=stest
+    )
+
+
+def sensitive_zhang_simple():
+    """
+    Simplifaction of the Zhang dataset
+    """
+    # parameters
+    n_all = 3000
+    num_train = 2000
+    num_inducing = 500
+    latent_std = 1.  # standard dev of the latent variable that we're trying to predict. bigger -> easier to predict
+    features_std = 1.  # standard dev of the features with respect to latent var. smaller -> easier to predict
+    sensitive_effect = .5  # effect of the sensitive attribute on the label. smaller -> easier to predict
+    raw_output_std = 1.  # standard dev of the raw output with respect to latent var. smaller -> easier to predict
+
+    # construction
+    random.seed(SEED)  # make it reproducible
+    sensitive_attr = random.random_integers(0, 1, n_all)
+    latent = random.normal(0, latent_std, n_all)
+    features = random.normal(latent, features_std)
+    raw_output = random.normal(latent + sensitive_effect * sensitive_attr, raw_output_std)
+    labels = (raw_output > 0).astype(np.int)
+
+    (xtrain, ytrain, strain), (xtest, ytest, stest) = select_training_and_test(
+        num_train, features[..., np.newaxis], labels[..., np.newaxis], sensitive_attr[..., np.newaxis])
+
+    sensitive_statistics(ytrain, strain, ytest, stest)
+
+    return Dataset(
+        train_fn=to_tf_dataset_fn(xtrain, ytrain, strain),
+        test_fn=to_tf_dataset_fn(xtest, ytest, stest),
+        num_train=num_train,
+        input_dim=2,
+        inducing_inputs=np.concatenate((xtrain[::num_train // num_inducing], strain[::num_train // num_inducing]), -1),
+        output_dim=1,
+        lik="LikelihoodLogistic",
+        metric="logistic_accuracy,pred_rate_y1_s0,pred_rate_y1_s1,base_rate_y1_s0,base_rate_y1_s1",
         xtrain=xtrain,
         ytrain=ytrain,
         xtest=xtest,
