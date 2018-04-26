f56f7a844e1ef342f2b102e7654fb736542daaca
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-04-26 17:01:02 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup config for installation"""
+from setuptools import setup
+
+setup(
+    name='universalgp',
+    version='0.1',
+    description='A basic generic implementation of Gaussian Processes',
+    url='https://github.com/predictive-analytics-lab/UniversalGP',
+    author='PAL group',
+    author_email='pal@sussex.ac.uk',
+    license='GPL-2.0',
+    packages=['universalgp'],
+)
