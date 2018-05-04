a51a4864b6aa19e4a9747d78a5df4c3224f9672a
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-04 14:26:34 +0100

    Add dataset that takes its data from a given numpy file
    The directory of the numpy file is given as a flag and is completely
    flexible. The numpy file has to contain `xtrain`, `ytrain`, `strain`,
    `xtest`, `ytest` and `stest`.
    
    There is also a JSON file with settings that has to be there, but that
    is maybe not really necessary.

--- universalgp/datasets/__init__.py
+++ universalgp/datasets/__init__.py
@@ -5,3 +5,4 @@ from .sensitive import sensitive_example
 from .sensitive_odds import sensitive_odds_example
 from .sensitive_zhang import sensitive_zhang, sensitive_zhang_simple
 from .flipped_labels import flipped_labels
+from .sensitive_from_numpy import sensitive_from_numpy
