a4021f8d5d69d84e05af2d587d96a311297b2afb
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-06 20:32:16 +0200

    Allow setting of path for prediction file

--- gaussian_process.py
+++ gaussian_process.py
@@ -34,7 +34,7 @@ tf.app.flags.DEFINE_string('save_dir', None,  # '/its/home/tk324/tensorflow/',
 tf.app.flags.DEFINE_string('plot', None, 'Which function to use for plotting (or None)')
 tf.app.flags.DEFINE_integer('logging_steps', 1, 'How many steps between logging the loss')
 tf.app.flags.DEFINE_string('gpus', '0', 'Which GPUs to use (should normally only be one)')
-tf.app.flags.DEFINE_boolean('save_preds', False, 'Whether to save the predictions for the test data')
+tf.app.flags.DEFINE_string('preds_path', '', 'Path where the predictions for the test data will be save (or "")')
 
 # you can specify a flag file here where you can put your flags instead of passing them from the command line
 FLAGFILE = ""  # "scripts/flagfiles/simple_example.sh"
