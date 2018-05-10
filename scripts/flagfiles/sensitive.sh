21d72b58f63a87722e8c776f5d37e03d3f0610bc
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-10 12:22:29 +0100

    Add metrics for equalized odds

--- scripts/flagfiles/sensitive.sh
+++ scripts/flagfiles/sensitive.sh
@@ -13,6 +13,10 @@
 # --cov=Linear
 --target_rate1=0.7
 --target_rate2=0.3
+--p_ybary0_s0=0.93
+--p_ybary1_s0=.6
+--p_ybary0_s1=.67
+--p_ybary1_s1=.6
 --biased_acceptance1=0.288
 --biased_acceptance2=0.760
 # --save_dir=/its/home/tk324/tensorflow/
