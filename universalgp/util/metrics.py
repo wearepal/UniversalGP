6d4c18d55a147d65303d9b55f677d5053ba9b29e
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-07-25 14:17:55 +0100

    Restrict the line length to 100 in some files (#71)

--- universalgp/util/metrics.py
+++ universalgp/util/metrics.py
@@ -35,7 +35,8 @@ def init_metrics(metric_flag, is_eager):
         metric_list = metric_flag.split(',')  # Split `metric_flag` into a list
     for name in metric_list:
         try:
-            metric = dict_of_metrics[name]  # Now we can just look up the metrics in the dictionary we created
+            # Now we can just look up the metrics in the dictionary we created
+            metric = dict_of_metrics[name]
         except KeyError:  # No metric found with the name `name`
             raise ValueError(f"Unknown metric \"{name}\"")
         metrics[name] = metric(is_eager)
@@ -96,7 +97,8 @@ class Metric:
         pass
 
     def _return_and_store(self, metric_op):
-        """In graph mode stores the result in `self.result` and returns the op so that it can be updated
+        """In graph mode stores the result in `self.result` and returns the op so that it can be
+        updated
 
         Does currently nothing in eager mode.
         """
@@ -153,7 +155,8 @@ class SoftAccuracy(Metric):
         self.accuracy = tfe.metrics.Accuracy() if is_eager else tf.metrics.accuracy
 
     def update(self, features, labels, pred_mean):
-        return self._return_and_store(self.accuracy(tf.argmax(labels, axis=1), tf.argmax(pred_mean, axis=1)))
+        return self._return_and_store(self.accuracy(tf.argmax(labels, axis=1),
+                                                    tf.argmax(pred_mean, axis=1)))
 
     def record(self):
         if self.is_eager:
@@ -167,7 +170,8 @@ class LogisticAccuracy(SoftAccuracy):
     name = "logistic_accuracy"
 
     def update(self, features, labels, pred_mean):
-        return self._return_and_store(self.accuracy(tf.cast(labels, tf.int32), tf.cast(pred_mean > 0.5, tf.int32)))
+        return self._return_and_store(self.accuracy(tf.cast(labels, tf.int32),
+                                                    tf.cast(pred_mean > 0.5, tf.int32)))
 
 
 class LogisticAccuracyYbar(SoftAccuracy):
@@ -175,7 +179,8 @@ class LogisticAccuracyYbar(SoftAccuracy):
     name = "logistic_accuracy_ybar"
 
     def update(self, features, labels, pred_mean):
-        return self._return_and_store(self.accuracy(features['ybar'], tf.cast(pred_mean > 0.5, tf.float32)))
+        return self._return_and_store(self.accuracy(features['ybar'],
+                                                    tf.cast(pred_mean > 0.5, tf.float32)))
 
 
 class PredictionRateY1S0(Mae):
@@ -183,7 +188,8 @@ class PredictionRateY1S0(Mae):
     name = "pred_rate_y1_s0"
 
     def update(self, features, labels, pred_mean):
-        accepted = tf.gather_nd(tf.cast(pred_mean > 0.5, tf.float32), tf.where(tf.equal(features['sensitive'], 0)))
+        accepted = tf.gather_nd(tf.cast(pred_mean > 0.5, tf.float32),
+                                tf.where(tf.equal(features['sensitive'], 0)))
         return self._return_and_store(self.mean(accepted))
 
 
@@ -192,7 +198,8 @@ class PredictionRateY1S1(Mae):
     name = "pred_rate_y1_s1"
 
     def update(self, features, labels, pred_mean):
-        accepted = tf.gather_nd(tf.cast(pred_mean > 0.5, tf.float32), tf.where(tf.equal(features['sensitive'], 1)))
+        accepted = tf.gather_nd(tf.cast(pred_mean > 0.5, tf.float32),
+                                tf.where(tf.equal(features['sensitive'], 1)))
         return self._return_and_store(self.mean(accepted))
 
 
@@ -219,7 +226,8 @@ class PredictionOddsYYbar1S0(Mae):
     name = "pred_odds_yybar1_s0"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar1_s0 = tf.logical_and(tf.equal(features['ybar'], 1), tf.equal(features['sensitive'], 0))
+        test_for_ybar1_s0 = tf.logical_and(tf.equal(features['ybar'], 1),
+                                           tf.equal(features['sensitive'], 0))
         accepted = tf.gather_nd(tf.cast(pred_mean > 0.5, tf.float32), tf.where(test_for_ybar1_s0))
         return self._return_and_store(self.mean(accepted))
 
@@ -229,7 +237,8 @@ class PredictionOddsYYbar1S1(Mae):
     name = "pred_odds_yybar1_s1"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar1_s1 = tf.logical_and(tf.equal(features['ybar'], 1), tf.equal(features['sensitive'], 1))
+        test_for_ybar1_s1 = tf.logical_and(tf.equal(features['ybar'], 1),
+                                           tf.equal(features['sensitive'], 1))
         accepted = tf.gather_nd(tf.cast(pred_mean > 0.5, tf.float32), tf.where(test_for_ybar1_s1))
         return self._return_and_store(self.mean(accepted))
 
@@ -239,7 +248,8 @@ class BaseOddsYYbar1S0(Mae):
     name = "base_odds_yybar1_s0"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar1_s0 = tf.logical_and(tf.equal(features['ybar'], 1), tf.equal(features['sensitive'], 0))
+        test_for_ybar1_s0 = tf.logical_and(tf.equal(features['ybar'], 1),
+                                           tf.equal(features['sensitive'], 0))
         accepted = tf.gather_nd(labels, tf.where(test_for_ybar1_s0))
         return self._return_and_store(self.mean(accepted))
 
@@ -249,7 +259,8 @@ class BaseOddsYYbar1S1(Mae):
     name = "base_odds_yybar1_s1"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar1_s1 = tf.logical_and(tf.equal(features['ybar'], 1), tf.equal(features['sensitive'], 1))
+        test_for_ybar1_s1 = tf.logical_and(tf.equal(features['ybar'], 1),
+                                           tf.equal(features['sensitive'], 1))
         accepted = tf.gather_nd(labels, tf.where(test_for_ybar1_s1))
         return self._return_and_store(self.mean(accepted))
 
@@ -259,7 +270,8 @@ class PredictionOddsYYbar0S0(Mae):
     name = "pred_odds_yybar0_s0"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar0_s0 = tf.logical_and(tf.equal(features['ybar'], 0), tf.equal(features['sensitive'], 0))
+        test_for_ybar0_s0 = tf.logical_and(tf.equal(features['ybar'], 0),
+                                           tf.equal(features['sensitive'], 0))
         accepted = tf.gather_nd(tf.cast(pred_mean < 0.5, tf.float32), tf.where(test_for_ybar0_s0))
         return self._return_and_store(self.mean(accepted))
 
@@ -269,7 +281,8 @@ class PredictionOddsYYbar0S1(Mae):
     name = "pred_odds_yybar0_s1"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar0_s1 = tf.logical_and(tf.equal(features['ybar'], 0), tf.equal(features['sensitive'], 1))
+        test_for_ybar0_s1 = tf.logical_and(tf.equal(features['ybar'], 0),
+                                           tf.equal(features['sensitive'], 1))
         accepted = tf.gather_nd(tf.cast(pred_mean < 0.5, tf.float32), tf.where(test_for_ybar0_s1))
         return self._return_and_store(self.mean(accepted))
 
@@ -279,7 +292,8 @@ class BaseOddsYYbar0S0(Mae):
     name = "base_odds_yybar0_s0"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar0_s0 = tf.logical_and(tf.equal(features['ybar'], 0), tf.equal(features['sensitive'], 0))
+        test_for_ybar0_s0 = tf.logical_and(tf.equal(features['ybar'], 0),
+                                           tf.equal(features['sensitive'], 0))
         accepted = tf.gather_nd(1 - labels, tf.where(test_for_ybar0_s0))
         return self._return_and_store(self.mean(accepted))
 
@@ -289,7 +303,8 @@ class BaseOddsYYbar0S1(Mae):
     name = "base_odds_yybar0_s1"
 
     def update(self, features, labels, pred_mean):
-        test_for_ybar0_s1 = tf.logical_and(tf.equal(features['ybar'], 0), tf.equal(features['sensitive'], 1))
+        test_for_ybar0_s1 = tf.logical_and(tf.equal(features['ybar'], 0),
+                                           tf.equal(features['sensitive'], 1))
         accepted = tf.gather_nd(1 - labels, tf.where(test_for_ybar0_s1))
         return self._return_and_store(self.mean(accepted))
 
@@ -332,3 +347,17 @@ class PredictionOddsYhatY0S1(Mae):
         test_for_y0_s1 = tf.logical_and(tf.equal(labels, 0), tf.equal(features['sensitive'], 1))
         accepted = tf.gather_nd(tf.cast(pred_mean < 0.5, tf.float32), tf.where(test_for_y0_s1))
         return self._return_and_store(self.mean(accepted))
+
+
+def mask_for(features, **kwargs):
+    """Create a 'mask' that filters for certain values
+
+    Args:
+        features: a dictionary of tensors
+        **kwargs: entries of the dictionary with the values, only the first two are used
+    Returns:
+        a mask
+    """
+    entries = list(kwargs.items())
+    return tf.where(tf.logical_and(tf.equal(features[entries[0][0]], entries[0][1]),
+                                   tf.equal(features[entries[1][0]], entries[1][1])))
