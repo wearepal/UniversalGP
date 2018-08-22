Fix serious errors in precision targeting code
I treated it like there were two degrees of freedom when in reality
there is only one.

Finally added a correct test.

test/test_vi_fair.py
@@ -48,7 +48,8 @@ def construct(*, p_y0_ybar0_s0, p_y1_ybar1_s0, p_y0_ybar0_s1, p_y1_ybar1_s1):
 
 
 class TestDebiasParams:
-    def test_extreme1(self):
+    @staticmethod
+    def test_extreme1():
         actual = construct_simple_full(0.7, 0.7, 0.7, 0.7)._debiasing_parameters().numpy()
         correct = construct(p_y0_ybar0_s0=1.,
                             p_y1_ybar1_s0=1.,
@@ -56,7 +57,8 @@ class TestDebiasParams:
                             p_y1_ybar1_s1=1.)
         np.testing.assert_allclose(actual, correct, RTOL)
 
-    def test_extreme2(self):
+    @staticmethod
+    def test_extreme2():
         actual = construct_simple_full(0.5, 0.5, 1e-5, 1 - 1e-5)._debiasing_parameters().numpy()
         correct = construct(p_y0_ybar0_s0=.5,
                             p_y1_ybar1_s0=1.,
@@ -64,7 +66,8 @@ class TestDebiasParams:
                             p_y1_ybar1_s1=.5)
         np.testing.assert_allclose(actual, correct, RTOL)
 
-    def test_moderate1(self):
+    @staticmethod
+    def test_moderate1():
         actual = construct_simple_full(0.3, 0.7, 0.5, 0.5)._debiasing_parameters().numpy()
         correct = construct(p_y0_ybar0_s0=1.,
                             p_y1_ybar1_s0=.3 / .5,
@@ -72,15 +75,29 @@ class TestDebiasParams:
                             p_y1_ybar1_s1=1.)
         np.testing.assert_allclose(actual, correct, RTOL)
 
-    def test_precision_target(self):
-        obj = construct_simple_full(0.3, 0.7, 0.5, 0.5, 0.7, 0.7)
-        actual_lik = obj._label_likelihood([.5, .5], [.5, .5])
-        np.testing.assert_allclose(actual_lik, [[1., 1.], [1., 1.]])
-        actual_full = obj._debiasing_parameters()
-        correct = construct(p_y0_ybar0_s0=1.,
-                            p_y1_ybar1_s0=.3 / .5,
-                            p_y0_ybar0_s1=1 - (.7 - .5) / .5,
-                            p_y1_ybar1_s1=1.)
+    @staticmethod
+    def test_precision_target():
+        p_y1_s0 = .3
+        p_y1_s1 = .9
+        p_ybar1_s0 = .5
+        p_ybar1_s1 = .6
+        prec_s0 = .7
+        prec_s1 = .8
+        obj = construct_simple_full(p_y1_s0, p_y1_s1, p_ybar1_s0, p_ybar1_s1, prec_s0, prec_s1)
+        actual_lik = obj._label_likelihood([p_y1_s0, p_y1_s1], [p_ybar1_s0, p_ybar1_s1])
+        np.testing.assert_allclose(actual_lik,
+                                   [[(p_ybar1_s0 - prec_s0 * p_y1_s0) / (1 - p_y1_s0),
+                                     1 - .8],
+                                    [.7,
+                                     (p_ybar1_s1 - (1 - prec_s1) * (1 - p_y1_s1)) / p_y1_s1]],
+                                   RTOL)
+        actual_full = obj._debiasing_parameters().numpy()
+        correct = construct(p_y0_ybar0_s0=1 - (1 - prec_s0) * p_y1_s0 / (1 - p_ybar1_s0),
+                            p_y1_ybar1_s0=prec_s0 * p_y1_s0 / p_ybar1_s0,
+                            p_y0_ybar0_s1=prec_s1 * (1 - p_y1_s1) / (1 - p_ybar1_s1),
+                            p_y1_ybar1_s1=1 - (1 - prec_s1) * (1 - p_y1_s1) / p_ybar1_s1)
+        print(actual_full)
+        print(correct)
         np.testing.assert_allclose(actual_full, correct, RTOL)
 
 

universalgp/inf/inf_vi_ybar.py
@@ -104,10 +104,12 @@ class VariationalYbar(Variational):
         # P(y'=1|s)
         target_acceptance = np.array([self.args['target_rate1'], self.args['target_rate2']])
         # P(y=1|s)
-        biased_acceptance = np.array([biased_acceptance1, biased_acceptance2])
+        positive_prior = np.array([biased_acceptance1, biased_acceptance2])
         # P(y'=1|y,s) shape: (y, s)
-        positive_value = self._label_likelihood(biased_acceptance, target_acceptance)
-        return compute_label_posterior(positive_value, biased_acceptance)
+        positive_value = self._label_likelihood(positive_prior, target_acceptance)
+        # P(y'|s) shape: (s, y')
+        label_evidence = np.stack([1 - target_acceptance, target_acceptance], axis=-1)
+        return compute_label_posterior(positive_value, positive_prior, label_evidence)
 
     def _label_likelihood(self, biased_acceptance, target_acceptance):
         """Compute the label likelihood (for positive labels)
@@ -118,17 +120,21 @@ class VariationalYbar(Variational):
         Returns:
             P(y'=1|y,s) with shape (y, s)
         """
-        precision = []
+        positive_lik = []
         for i, (target, biased) in enumerate(zip(target_acceptance, biased_acceptance)):
             if target > biased:
-                p_ybary0 = (1 - target) / (1 - biased)
+                # P(y'=1|y=1)
                 p_ybary1 = self.args[f"p_ybary0_or_ybary1_s{i}"]
+                # P(y'=1|y=0) = (P(y'=1) - P(y'=1|y=1)P(y=1))/P(y=0)
+                p_ybar1_y0 = (target - p_ybary1 * biased) / (1 - biased)
             else:
-                p_ybary0 = self.args[f"p_ybary0_or_ybary1_s{i}"]
-                p_ybary1 = target / biased
-            precision.append([1 - p_ybary0, p_ybary1])
-        precision_arr = np.array(precision)  # shape: (s, y)
-        return np.transpose(precision_arr)  # shape: (y, s)
+                p_ybar1_y0 = 1 - self.args[f"p_ybary0_or_ybary1_s{i}"]
+                # P(y'=1|y=0) = (P(y'=1) - P(y'=1|y=0)P(y=0))/P(y=1)
+                p_ybary1 = (target - p_ybar1_y0 * (1 - biased)) / biased
+            positive_lik.append([p_ybar1_y0, p_ybary1])
+        positive_lik_arr = np.array(positive_lik)  # shape: (s, y)
+        print(positive_lik_arr)
+        return np.transpose(positive_lik_arr)  # shape: (y, s)
 
 
 class VariationalYbarEqOdds(VariationalYbar):
@@ -150,7 +156,7 @@ class VariationalYbarEqOdds(VariationalYbar):
         return compute_label_posterior(positive_value, positive_prior)
 
 
-def compute_label_posterior(positive_value, positive_prior):
+def compute_label_posterior(positive_value, positive_prior, label_evidence=None):
     """Return label posterior from positive likelihood P(y'=1|y,s) and positive prior P(y=1|s)
 
     Args:
@@ -173,7 +179,8 @@ def compute_label_posterior(positive_value, positive_prior):
     # P(y',y|s) shape: (y, s, y')
     joint = label_likelihood * label_prior
     # P(y'|s) shape: (s, y')
-    label_evidence = np.sum(joint, axis=0)
+    if label_evidence is None:
+        label_evidence = np.sum(joint, axis=0)
 
     # compute posterior
     # P(y|y',s) shape: (y, s, y')