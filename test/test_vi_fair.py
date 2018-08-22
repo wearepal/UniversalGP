Correct implementation of precision targeting (#72)
* Attempt at better precision targeting options

* Fix serious errors in precision targeting code

I treated it like there were two degrees of freedom when in reality
there is only one.

Finally added a correct test.

test/test_vi_fair.py
@@ -14,16 +14,20 @@ RTOL = 1e-4
 PARAMS = {'num_components': 1, 'diag_post': False}
 
 
-def construct_simple_full(biased_acceptance1, biased_acceptance2, target_rate1, target_rate2):
+def construct_simple_full(biased_rate1, biased_rate2, target_rate1, target_rate2,
+                          p_ybary0_or_ybary1_s0=1.0, p_ybary0_or_ybary1_s1=1.0):
     likelihood = lik.LikelihoodGaussian({'sn': 1.0})
     kernel = [cov.SquaredExponential(input_dim=1, args=dict(length_scale=1.0, sf=1.0, iso=False))]
     # In most of our unit test, we will replace this value with something else.
     return inference.VariationalYbar(kernel, likelihood, 1, 1, {
-        **PARAMS, 'target_rate1': target_rate1, 'target_rate2': target_rate2, 'biased_acceptance1': biased_acceptance1,
-        'biased_acceptance2': biased_acceptance2, 'probs_from_flipped': False})
+        **PARAMS, **dict(target_rate1=target_rate1, target_rate2=target_rate2,
+                         biased_acceptance1=biased_rate1, biased_acceptance2=biased_rate2,
+                         probs_from_flipped=False, p_ybary0_or_ybary1_s0=p_ybary0_or_ybary1_s0,
+                         p_ybary0_or_ybary1_s1=p_ybary0_or_ybary1_s1)})
 
 
-def construct_eq_odds(biased_acceptance1, biased_acceptance2, p_ybary0_s0, p_ybary1_s0, p_ybary0_s1, p_ybary1_s1):
+def construct_eq_odds(biased_acceptance1, biased_acceptance2, p_ybary0_s0, p_ybary1_s0, p_ybary0_s1,
+                      p_ybary1_s1):
     likelihood = lik.LikelihoodGaussian({'sn': 1.0})
     kernel = [cov.SquaredExponential(input_dim=1, args=dict(length_scale=1.0, sf=1.0, iso=False))]
     # In most of our unit test, we will replace this value with something else.
@@ -44,7 +48,8 @@ def construct(*, p_y0_ybar0_s0, p_y1_ybar1_s0, p_y0_ybar0_s1, p_y1_ybar1_s1):
 
 
 class TestDebiasParams:
-    def test_extreme1(self):
+    @staticmethod
+    def test_extreme1():
         actual = construct_simple_full(0.7, 0.7, 0.7, 0.7)._debiasing_parameters().numpy()
         correct = construct(p_y0_ybar0_s0=1.,
                             p_y1_ybar1_s0=1.,
@@ -52,7 +57,8 @@ class TestDebiasParams:
                             p_y1_ybar1_s1=1.)
         np.testing.assert_allclose(actual, correct, RTOL)
 
-    def test_extreme2(self):
+    @staticmethod
+    def test_extreme2():
         actual = construct_simple_full(0.5, 0.5, 1e-5, 1 - 1e-5)._debiasing_parameters().numpy()
         correct = construct(p_y0_ybar0_s0=.5,
                             p_y1_ybar1_s0=1.,
@@ -60,13 +66,39 @@ class TestDebiasParams:
                             p_y1_ybar1_s1=.5)
         np.testing.assert_allclose(actual, correct, RTOL)
 
-    def test_moderate1(self):
+    @staticmethod
+    def test_moderate1():
         actual = construct_simple_full(0.3, 0.7, 0.5, 0.5)._debiasing_parameters().numpy()
         correct = construct(p_y0_ybar0_s0=1.,
                             p_y1_ybar1_s0=.3 / .5,
                             p_y0_ybar0_s1=1 - (.7 - .5) / .5,
                             p_y1_ybar1_s1=1.)
         np.testing.assert_allclose(actual, correct, RTOL)
+
+    @staticmethod
+    def test_precision_target():
+        p_y1_s0 = .3
+        p_y1_s1 = .9
+        p_ybar1_s0 = .5
+        p_ybar1_s1 = .6
+        prec_s0 = .7
+        prec_s1 = .8
+        obj = construct_simple_full(p_y1_s0, p_y1_s1, p_ybar1_s0, p_ybar1_s1, prec_s0, prec_s1)
+        actual_lik = obj._label_likelihood([p_y1_s0, p_y1_s1], [p_ybar1_s0, p_ybar1_s1])
+        np.testing.assert_allclose(actual_lik,
+                                   [[(p_ybar1_s0 - prec_s0 * p_y1_s0) / (1 - p_y1_s0),
+                                     1 - .8],
+                                    [.7,
+                                     (p_ybar1_s1 - (1 - prec_s1) * (1 - p_y1_s1)) / p_y1_s1]],
+                                   RTOL)
+        actual_full = obj._debiasing_parameters().numpy()
+        correct = construct(p_y0_ybar0_s0=1 - (1 - prec_s0) * p_y1_s0 / (1 - p_ybar1_s0),
+                            p_y1_ybar1_s0=prec_s0 * p_y1_s0 / p_ybar1_s0,
+                            p_y0_ybar0_s1=prec_s1 * (1 - p_y1_s1) / (1 - p_ybar1_s1),
+                            p_y1_ybar1_s1=1 - (1 - prec_s1) * (1 - p_y1_s1) / p_ybar1_s1)
+        print(actual_full)
+        print(correct)
+        np.testing.assert_allclose(actual_full, correct, RTOL)
 
 
 class TestEqOddsParams: