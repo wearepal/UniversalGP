0e1004eebed34defae43bc1860a561bcfd852680
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-08-29 16:50:42 +0100

    Add logistic regression algorithm that enforces eq opp

--- universalgp/inf/logistic_regression.py
+++ universalgp/inf/logistic_regression.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 
-from .inf_vi_ybar import VariationalYbar
+from .inf_vi_ybar import VariationalWithS, VariationalYbar, VariationalYbarEqOdds
 
 
 class LogisticRegressionModel:
@@ -49,12 +49,13 @@ class LogisticRegressionModel:
         return self._model.variables
 
 
-class LogReg(VariationalYbar):
+# TODO: let FairLogReg and EqOppLogReg be descendants of LogReg. all that's needed is to make the
+#       `_debiasing_parameters()` functions separate from their classes.
+class LogReg(VariationalWithS):
     """Simple logistic regression model"""
     def __init__(self, cov_func, lik_func, num_train, inducing_inputs, args):
-        input_dim = cov_func[0].input_dim
         self.args = args
-        self.lr = LogisticRegressionModel(args['s_as_input'], input_dim)
+        self.lr = LogisticRegressionModel(args['s_as_input'], cov_func[0].input_dim)
 
     def inference(self, features, outputs, _):
         return self.lr.loss(features, outputs)
@@ -69,32 +70,51 @@ class LogReg(VariationalYbar):
 class FairLogReg(VariationalYbar):
     """Fair logistic regression"""
     def __init__(self, cov_func, lik_func, num_train, inducing_inputs, args):
-        input_dim = cov_func[0].input_dim
         self.args = args
-        self.lr = LogisticRegressionModel(args['s_as_input'], input_dim)
+        self.lr = LogisticRegressionModel(args['s_as_input'], cov_func[0].input_dim)
 
     def inference(self, features, outputs, is_train):
-        if is_train:
-            sens_attr = tf.cast(tf.squeeze(features['sensitive'], -1), dtype=tf.int32)
-            out_int = tf.cast(tf.squeeze(outputs, -1), dtype=tf.int32)
-            # likelihood for y=1
-            lik1 = tf.squeeze(self.lr.predict(features)[0], axis=-1)
-            # likelihood for y=0
-            lik0 = 1 - lik1
-            lik = tf.stack((lik0, lik1), axis=-1)
-            debias = self._debiasing_parameters()
-            # `debias` has the shape (y, s, y'). we stack output and sensitive to (batch_size, 2)
-            # then we use the last 2 values of that as indices for `debias`
-            # shape of debias_per_example: (batch_size, output_dim, 2)
-            debias_per_example = tf.gather_nd(debias, tf.stack((out_int, sens_attr), axis=-1))
-            weighted_lik = debias_per_example * lik
-            log_cond_prob = tf.log(tf.reduce_sum(weighted_lik, axis=-1))
-            loss = -tf.reduce_mean(log_cond_prob)
-            return {'loss': loss}, self.lr.get_trainable_variables()
-        return self.lr.loss(features, outputs)
+        return ybar_inference(features, outputs, is_train, self._debiasing_parameters(), self.lr)
 
     def predict(self, test_inputs):
         return self.lr.predict(test_inputs)
 
     def get_all_variables(self):
         return self.lr.get_all_variables()
+
+
+class EqOppLogReg(VariationalYbarEqOdds):
+    """Fair logistic regression"""
+    def __init__(self, cov_func, lik_func, num_train, inducing_inputs, args):
+        self.args = args
+        self.lr = LogisticRegressionModel(args['s_as_input'], cov_func[0].input_dim)
+
+    def inference(self, features, outputs, is_train):
+        return ybar_inference(features, outputs, is_train, self._debiasing_parameters(), self.lr)
+
+    def predict(self, test_inputs):
+        return self.lr.predict(test_inputs)
+
+    def get_all_variables(self):
+        return self.lr.get_all_variables()
+
+
+def ybar_inference(features, outputs, is_train, debias, lr):
+    """Inference for targeting ybar"""
+    if is_train:
+        sens_attr = tf.cast(tf.squeeze(features['sensitive'], -1), dtype=tf.int32)
+        out_int = tf.cast(tf.squeeze(outputs, -1), dtype=tf.int32)
+        # likelihood for y=1
+        lik1 = tf.squeeze(lr.predict(features)[0], axis=-1)
+        # likelihood for y=0
+        lik0 = 1 - lik1
+        lik = tf.stack((lik0, lik1), axis=-1)
+        # `debias` has the shape (y, s, y'). we stack output and sensitive to (batch_size, 2)
+        # then we use the last 2 values of that as indices for `debias`
+        # shape of debias_per_example: (batch_size, output_dim, 2)
+        debias_per_example = tf.gather_nd(debias, tf.stack((out_int, sens_attr), axis=-1))
+        weighted_lik = debias_per_example * lik
+        log_cond_prob = tf.log(tf.reduce_sum(weighted_lik, axis=-1))
+        loss = -tf.reduce_mean(log_cond_prob)
+        return {'loss': loss}, lr.get_trainable_variables()
+    return lr.loss(features, outputs)
