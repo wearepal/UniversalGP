7c26461946c9da674d7ad9493fd40437acb1ffea
Magica_Chen <sxtpy2010@gmail.com>
2018-12-22 15:57:22 +0000

    update setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,15 @@ setup(
     description='A basic generic implementation of Gaussian Processes',
     url='https://github.com/predictive-analytics-lab/UniversalGP',
     author='PAL group',
+    keywords="Gaussian process",
     author_email='pal@sussex.ac.uk',
     license='GPL-2.0',
-    packages=['universalgp'],
+    packages=['universalgp',
+              'universalgp.cov',
+              'universalgp.mean',
+              'universalgp.datasets',
+              'universalgp.inf',
+              'universalgp.lik',
+              'universalgp.util'],
+
 )
