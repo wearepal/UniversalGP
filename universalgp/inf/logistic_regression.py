67254c81314f1574cf20178d6fbc3a11dcf182a9
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-01-02 18:42:33 +0000

    Use logsumexp to reduce numerical instability

--- universalgp/inf/logistic_regression.py
+++ universalgp/inf/logistic_regression.py
@@ -2,7 +2,6 @@
 
 import tensorflow as tf
 from tensorflow import manip as tft
-from tensorflow import math as tfm
 
 from .inf_vi_ybar import (sensitive_prediction, construct_input, debiasing_params_target_rate,
                           debiasing_params_target_tpr)
@@ -62,27 +61,30 @@ class FairLogReg(LogReg):
             return super().inference(features, outputs, is_train)
         sens_attr = tf.cast(tf.squeeze(features['sensitive'], -1), dtype=tf.int32)
         out_int = tf.cast(tf.squeeze(outputs, -1), dtype=tf.int32)
-        # likelihood for y=1
-        lik1 = tf.squeeze(tf.nn.sigmoid(self._model(self._get_inputs(features))), axis=-1)
-        # likelihood for y=0
-        lik0 = 1 - lik1
-        lik = tf.stack((lik0, lik1), axis=-1)
-        debias = self._debiasing_parameters()
-        # `debias` has the shape (y, s, y'). we stack output and sensitive to (batch_size, 2)
-        # then we use the last 2 values of that as indices for `debias`
-        # shape of debias_per_example: (batch_size, output_dim, 2)
-        debias_per_example = tft.gather_nd(debias, tf.stack((out_int, sens_attr), axis=-1))
-        weighted_lik = debias_per_example * lik
-        log_cond_prob = tfm.log(tf.reduce_sum(weighted_lik, axis=-1))
+        # output
+        logits = tf.squeeze(self._model(self._get_inputs(features)), axis=-1)
+        # log likelihood for y=1
+        log_lik1 = tf.log_sigmoid(logits)
+        # log likelihood for y=0
+        log_lik0 = tf.log_sigmoid(-logits)
+        log_lik = tf.stack((log_lik0, log_lik1), axis=-1)
+        log_debias = self._log_debiasing_parameters()
+        # `log_debias` has the shape (y, s, y'). we stack output and sensitive to (batch_size, 2)
+        # then we use the last 2 values of that as indices for `log_debias`
+        # shape of log_debias_per_example: (batch_size, output_dim, 2)
+        log_debias_per_example = tft.gather_nd(log_debias, tf.stack((out_int, sens_attr), axis=-1))
+        weighted_log_lik = log_debias_per_example + log_lik
+        # logsumexp is numerically stable
+        log_cond_prob = tf.reduce_logsumexp(weighted_log_lik, axis=-1)
         regr_loss = -tf.reduce_mean(log_cond_prob)
         l2_loss = self._l2_loss()
         return {'loss': regr_loss + l2_loss, 'regr_loss': regr_loss, 'l2_loss': l2_loss}
 
-    def _debiasing_parameters(self):
+    def _log_debiasing_parameters(self):
         return debiasing_params_target_rate(self.args)
 
 
 class EqOddsLogReg(FairLogReg):
     """Fair logistic regression for equalized odds (or equality of opportunity)"""
-    def _debiasing_parameters(self):
+    def _log_debiasing_parameters(self):
         return debiasing_params_target_tpr(self.args)
